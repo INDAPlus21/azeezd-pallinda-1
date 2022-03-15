7fc47881264e6f5252568016b120c7ff7e71931e
AzeezDaoud <azeez.daoud3@gmail.com>
2022-03-15 22:51:58 +0100

    Restructure files simiilar to task folder

--- src/gotour_exercises/maps.go
+++ src/gotour_exercises/maps.go
@@ -10,11 +10,7 @@ func WordCount(s string) map[string]int {
 	words := strings.Fields(s)
 
 	for _, word := range words {
-		if _, exists := wordCount[word]; exists { // Word in map, increment counter
-			wordCount[word]++
-		} else { // Not in map, add it with default value 1
-			wordCount[word] = 1
-		}
+		wordCount[word]++
 	}
 
 	return wordCount
